aca5604151cb8b809c3978d0ec6d9cff31accba7
Gallien BOUDRIE <gboudrie@e1r2p10.42.fr>
2016-01-21 16:04:52 +0100

    njg

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 22:06:16 by gboudrie          #+#    #+#             */
-/*   Updated: 2015/12/20 18:03:58 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/01/21 16:04:33 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,24 @@
 
 char		*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	ft_strncpy(s1 + ft_strlen(s1), s2, n);
-	if (n <= ft_strlen(s2))
-		s1[ft_strlen(s1) + n] = '\0';
+	char		*ptr1;
+	char		*ptr2;
+
+	ptr1 = s1;
+	ptr2 = (char *)s2;
+	if ((n != 0) && (*ptr2 != '\0'))
+	{
+		while (*ptr1 != '\0')
+			ptr1++;
+		while ((n > 0) && (*ptr2 != '\0'))
+		{
+			*ptr1 = *ptr2;
+			ptr1++;
+			ptr2++;
+			n--;
+		}
+		if (*ptr1 != '\0')
+			*ptr1 = '\0';
+	}
 	return (s1);
 }
