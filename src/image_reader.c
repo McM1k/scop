2fb633ae8f06ca0bf79d8fdc39689b85299689a6
gboudrie <gboudrie@student.42.fr>
2020-07-20 12:58:28 +0200

    repairing bmp parser

--- src/image_reader.c
+++ src/image_reader.c
@@ -6,11 +6,81 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 14:34:05 by gboudrie          #+#    #+#             */
-/*   Updated: 2020/07/13 15:03:12 by gboudrie         ###   ########.fr       */
+/*   Updated: 2020/07/20 12:57:32 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+
+t_bmp		*get_bmp_info(char *header, t_bmp *bmp)
+{
+
+	ft_memcpy(bmp, header, 54);
+
+	ft_putstr("type: ");
+	ft_putchar(bmp->bfType[0]);
+	ft_putchar(bmp->bfType[1]);
+	ft_putchar('\n');
+	ft_putstr("bfSize: ");
+	ft_putnbr((bmp->bfSize));
+	ft_putchar('\n');
+	ft_putstr("bfReserved1: ");
+	ft_putnbr((int)(bmp->bfReserved1));
+	ft_putchar('\n');
+	ft_putstr("bfReserved2: ");
+	ft_putnbr((int)(bmp->bfReserved2));
+	ft_putchar('\n');
+	ft_putstr("offbits: ");
+	ft_putnbr(bmp->bfOffBits);
+	ft_putchar('\n');
+	
+	ft_putstr("biSize: ");
+	ft_putnbr((bmp->biSize));
+	ft_putchar('\n');
+	ft_putstr("width: ");
+	ft_putnbr(bmp->biWidth);
+	ft_putchar('\n');
+	ft_putstr("height: ");
+	ft_putnbr(bmp->biHeight);
+	ft_putchar('\n');
+	ft_putstr("biPlanes: ");
+	ft_putnbr((int)(bmp->biPlanes));
+	ft_putchar('\n');
+	ft_putstr("pixSize: ");
+	ft_putnbr((int)(bmp->biBitCount));
+	ft_putchar('\n');
+	ft_putstr("biCompression: ");
+	ft_putnbr((int)(bmp->biCompression));
+	ft_putchar('\n');
+	ft_putstr("biSizeImage: ");
+	ft_putnbr((int)(bmp->biSizeImage));
+	ft_putchar('\n');
+	ft_putstr("biXPelsPerMeter: ");
+	ft_putnbr((int)(bmp->biXPelsPerMeter));
+	ft_putchar('\n');
+	ft_putstr("biYPelsPerMeter: ");
+	ft_putnbr((int)(bmp->biYPelsPerMeter));
+	ft_putchar('\n');
+	ft_putstr("biClrUsed: ");
+	ft_putnbr((int)(bmp->biClrUsed));
+	ft_putchar('\n');
+	ft_putstr("biClrImportant: ");
+	ft_putnbr((int)(bmp->biClrImportant));
+	ft_putchar('\n');
+
+
+	if (bmp->bfType[0] != 'B' && bmp->bfType[1] != 'M')
+	{
+		error_callback(-101, "not a BM bitmap image");
+		return (NULL);
+	}
+	if (bmp->bfOffBits != 54)
+	{
+		error_callback(-102, "bad offBit");
+		return (NULL);
+	}
+	return (bmp);
+}
 
 t_bmp		*read_bmp(char *path)
 {
@@ -18,6 +88,7 @@ t_bmp		*read_bmp(char *path)
 	int		fd;
 	char	buf_head[54];
 	int		size;
+	int		ret;
 
 	if ((fd = open(path, O_RDONLY)) == -1)
 		return (NULL);
@@ -32,21 +103,5 @@ t_bmp		*read_bmp(char *path)
 	bmp.image[size] = '\0';
 	if (close(fd) == -1)
 		return (NULL);
-	return (&bmp);
-}
-
-t_bmp		*get_bmp_info(char *header, t_bmp *bmp)
-{
-	ft_memcpy(bmp, header, 54);
-	if (bmp->bfType[0] != 'B' && bmp->bfType[1] != 'M')
-	{
-		error_callback(-101, "not a BM bitmap image");
-		return (NULL);
-	}
-	if (bmp->bfOffBits != 54)
-	{
-		error_callback(-102, "bad offBit");
-		return (NULL);
-	}
-	return (bmp);
+	return (ft_memalloc(sizeof(t_bmp)));
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 15:52:12 by gboudrie          #+#    #+#             */
-/*   Updated: 2020/07/10 12:51:13 by gboudrie         ###   ########.fr       */
+/*   Updated: 2020/07/20 12:19:27 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,29 @@ int     main(int ac, char **av)
 		return(-1);
 	
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
-		0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// bottom left
-		-0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top left 
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 	unsigned int indices[] = {  // note that we start from 0!
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
 	};
-	float texCoords[] = {
-		0.0f, 0.0f,  // lower-left corner  
-		1.0f, 0.0f,  // lower-right corner
-		0.5f, 1.0f   // top-center corner
-	};
 
+	t_bmp *bmp = read_bmp("res/unicorn.bmp");
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	unsigned int texture;
+	glGenTextures(1, &texture);  
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, bmp->biWidth, bmp->biHeight,
+				 0, GL_RGB, GL_UNSIGNED_BYTE, bmp->image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -49,18 +54,23 @@ int     main(int ac, char **av)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);  
+	glGenVertexArrays(1, &VAO);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+						  (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
 						  (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+						  (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);  
+
 	unsigned int shaderProgram = init_shader_program();
 	
 	while(!glfwWindowShouldClose(window))
@@ -82,6 +92,8 @@ int     main(int ac, char **av)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
     }
+	ft_memdel((void **)(&(bmp->image)));
+	ft_memdel((void **)(&bmp));
     glfwTerminate();
     return(0);
 }
