cba5ae2941c6bae0edcc2d83aa654a4a5b223eb9
gboudrie <gboudrie@student.42.fr>
2020-09-03 18:56:21 +0200

    guefwhfrbhufrjbh

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -3,11 +3,17 @@ out vec4 FragColor;
 flat in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D tex1;
+uniform sampler2D tex2;
+uniform sampler2D tex3;
+uniform vec4 mixvalues;
 
 void main()
 {
 	float grey;
 	grey = (0.2125 * ourColor.x + 0.7154 * ourColor.y + 0.0721 * ourColor.z) / 1.2;
-	FragColor = texture(ourTexture, TexCoord);
+	FragColor = texture(tex1, TexCoord) * mixvalues.x
+			  + texture(tex2, TexCoord) * mixvalues.y
+			  + texture(tex3, TexCoord) * mixvalues.z
+			  + grey * mixvalues.w;
 }
